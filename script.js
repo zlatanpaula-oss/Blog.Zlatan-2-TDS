be35f6f04af0481b647e16f477155d895bbe95f3
zlatanpaula-oss <zlatan.paula@escola.pr.gov.br>
2026-09-04 08:48:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const artigos = document.querySelectorAll("article");
+
+artigos.forEach(function (artigo) {
+    const btnLike = artigo.querySelector(".btn-like");
+    const btnDislike = artigo.querySelector(".btn-dislike");
+    let statusReacao = null; // Guarda o estado atual do post ('like', 'dislike' ou null)
+
+    btnLike.addEventListener("click", function() {
+        let contadorLike = btnLike.querySelector("span");
+        let contadorDislike = btnDislike.querySelector("span");
+
+        if (statusReacao === "like") {
+            contadorLike.textContent = parseInt(contadorLike.textContent) - 1;
+            btnLike.classList.remove("ativo-like");
+            statusReacao = null;
+        } else {
+            if (statusReacao === "dislike") {
+                contadorDislike.textContent = parseInt(contadorDislike.textContent) - 1;
+                btnDislike.classList.remove("ativo-dislike");
+            }
+            contadorLike.textContent = parseInt(contadorLike.textContent) + 1;
+            btnLike.classList.add("ativo-like");
+            statusReacao = "like";
+        }
+    });
+
+    btnDislike.addEventListener("click", function() {
+        let contadorLike = btnLike.querySelector("span");
+        let contadorDislike = btnDislike.querySelector("span");
+
+        if (statusReacao === "dislike") {
+            contadorDislike.textContent = parseInt(contadorDislike.textContent) - 1;
+            btnDislike.classList.remove("ativo-dislike");
+            statusReacao = null;
+        } else {
+            if (statusReacao === "like") {
+                contadorLike.textContent = parseInt(contadorLike.textContent) - 1;
+                btnLike.classList.remove("ativo-like");
+            }
+            contadorDislike.textContent = parseInt(contadorDislike.textContent) + 1;
+            btnDislike.classList.add("ativo-dislike");
+            statusReacao = "dislike";
+        }
+    });
+});
